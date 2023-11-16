7093400a5ef8d697e0fef48fc44f38d89b647209
naksudev <xersinn@gmail.com>
2023-11-16 14:16:38 +0100

    :construction_worker: Testing requests

--- tests/test_sb.py
+++ tests/test_sb.py
@@ -1,6 +1,22 @@
 import pytest
-from modules import sb
+from unittest import mock
+from modules import webhook  # Importe ta fonction send depuis ton module
 
-def test_openAgenda():
-    result = sb.openAgenda()
-    assert isinstance(result, str)
+# Utilise un mock pour requests.post()
+@mock.patch('requests.post')
+def test_send(mock_post):
+    # Configuration du mock pour simuler une réponse 200
+    mock_post.return_value.status_code = 200
+
+    # Appel à la fonction send avec des valeurs factices
+    url = 'https://example.com/webhook'
+    imgPath = 'output/example.jpg'
+    webhook.send(url, imgPath)
+
+    # Vérification que requests.post() a été appelé avec les bonnes valeurs
+    mock_post.assert_called_once_with(
+        url,
+        data={'payload_json': mock.ANY},
+        files={'file': (imgPath, mock.ANY)}
+    )
+
