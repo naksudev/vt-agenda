cde75d4b721c52fd295d8b0152ea7747e92eb9b4
naksudev <xersinn@gmail.com>
2023-12-21 16:26:18 +0100

    :recycle: Much better refacto

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
-from modules import sb
 from modules import webhook
 
-if __name__ == '__main__':
-    # Select environement
+def select_env():
+    environments = {
+        "dev": "https://discord.com/api/webhooks/1170076204028678274/_gIwgnTt45boaik2q0hHn15EkyLyl-l0jIrtJw8y-oKUvihM5KSH65d9pUzXh3ifVDDY",
+        "prod": "https://discord.com/api/webhooks/1170093412343627796/gKBXnkcJkxYWq_JaTVLgsu5lsC579ZiN7nfqZtz3iLMYx7mEvJpG1F_4TLmtkC-pnEv4"
+    }
+
     while True:
         print("[?] Sélectionnez l'environnement webhook")
-        envInput = str(input("(dev) (prod) >> "))
+        env_input = input("(dev) (prod) >> ").lower()
 
-        if envInput == "dev":
-            url = "https://discord.com/api/webhooks/1170076204028678274/_gIwgnTt45boaik2q0hHn15EkyLyl-l0jIrtJw8y-oKUvihM5KSH65d9pUzXh3ifVDDY"
-            break
-        elif envInput == "prod":
-            url = "https://discord.com/api/webhooks/1170093412343627796/gKBXnkcJkxYWq_JaTVLgsu5lsC579ZiN7nfqZtz3iLMYx7mEvJpG1F_4TLmtkC-pnEv4"
-            break
+        if env_input in environments:
+            return environments[env_input]
         else:
             continue
 
-    # Main script
-    screenName = sb.openAgenda()
-    sb.cropImg(screenName)
-    webhook.send(url, screenName)
+def main():
+    try:
+        webhook_url = select_env()
 
+        print("[?] Entrez l'année voulue")
+        yearInput = int(input(">> "))
+
+        print("[?] Entrez la semaine voulue")
+        weekInput = int(input(">> "))
+
+        finalURL = f"https://edt.univ-evry.fr/vue_etudiant_horizontale.php?current_year={yearInput}&current_student=68425503&current_week={weekInput}&lar=1920&hau=1080"
+        webhook.send(webhook_url, finalURL)
+    except ValueError:
+        print("Could not convert data to an integer.")
+    except Exception as e:
+        print(f"Unexcepted error: {e}")
+        raise
+
+if __name__ == '__main__':
+    main()
