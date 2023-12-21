6a32462970785e70632787cfcc7948e856271f3c
naksudev <xersinn@gmail.com>
2023-12-21 16:55:23 +0100

    :bricks: No more Seleniumbase

--- modules/webhook.py
+++ modules/webhook.py
@@ -1,25 +1,18 @@
 import requests
 import json
 
-def send(url, imgPath):
+def send(url, contentPayload):
     payload = {
         "username": "VT Agenda",
         "avatar_url": "https://edt.univ-evry.fr/vt_agenda.png",
-        "content": "> [Accéder à VT](https://edt.univ-evry.fr/index.php) // `lpisvd_11`"
+        "content": f"{contentPayload}"
     }
 
-    files = { 'file': (imgPath, _getImageData(imgPath)) }
+    response = requests.post(url, data={'payload_json': json.dumps(payload)})
 
-    response = requests.post(url, data={'payload_json': json.dumps(payload)}, files=files)
-
-    if response.status_code == 200:
-        print("\n[!] Screenshot envoyé.")
+    if response.status_code == 200 or response.status_code == 204:
+        print("\n[!] Webhook envoyé (sans attachements)")
     else:
         print(f"\n[X] Erreur webhook ({response.status_code})")
         print(response.content)
 
-def _getImageData(imgPath):
-    with open(imgPath, 'rb') as img:
-        image_data = img.read()
-
-    return image_data
